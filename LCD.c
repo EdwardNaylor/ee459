42ab9c1e3b33047f936f93e0f2e2f997543df601
jason <kwakjason94@mgmail.com>
2017-03-21 16:50:59 -0700

    Added gitignore, added LCD lines

--- LCD.c
+++ LCD.c
@@ -24,10 +24,19 @@ void sci_init(void);
 void sci_out(unsigned char);
 void sci_outs(unsigned char *);
 
-unsigned char str1[] = "12345678901234567890";
+unsigned char str1[] = "1234567890";
 unsigned char str2[] = ">> USC EE459L <<";
 unsigned char str3[] = ">> at328-6.c <<<";
 unsigned char str4[] = "-- April 11, 2011 --";
+
+/*
+	First line:		0-19
+	Second line:	40-59
+	Third line:		20-39
+	Fourth line:	??-??
+
+	Fourth line doesnt seem to work well
+*/
 
 #define FOSC 7372800			// Clock frequency 7372800
 #define BAUD 9600              // Baud rate used by the LCD 104ns
@@ -35,20 +44,19 @@ unsigned char str4[] = "-- April 11, 2011 --";
 
 int main(void) {
 
-    sci_init();                 // Initialize the SCI port
-    
-    lcd_init();                 // Initialize the LCD
+	sci_init();						// Initialize the SCI port
 
-    lcd_out(0, (unsigned char *) str1);    // Print string on line 1
-	lcd_out(20, (unsigned char *) str1);
-//    lcd_out(2, 1, (unsigned char *) str2);    // Print string on line 2
-//    lcd_out(3, 1, (unsigned char *) str3);    // Print string on line 3
-//    lcd_out(4, 1, (unsigned char *) str4);    // Print string on line 4
+	lcd_init();						// Initialize the LCD
 
-    while (1) {               // Loop forever
-    }
+	lcd_out(55, (unsigned char *) str1);				// Print string on line 1
+	//lcd_out(2, 1, (unsigned char *) str2);		// Print string on line 2
+	//lcd_out(3, 1, (unsigned char *) str3);		// Print string on line 3
+	//lcd_out(4, 1, (unsigned char *) str4);		// Print string on line 4
 
-    return 0;   /* never reached */
+	while (1) {               // Loop forever
+	}
+
+	return 0;   /* never reached */
 }
 
 /*
@@ -56,10 +64,10 @@ int main(void) {
 */
 void lcd_init()
 {
-    _delay_ms(250);          // Wait 500msec for the LCD to start up
-    _delay_ms(250);
-    sci_out(0xfe);           // Clear the screen
-    sci_out(0x51);
+	_delay_ms(250);			// Wait 500msec for the LCD to start up
+	_delay_ms(250);
+	sci_out(0xfe);				// Clear the screen
+	sci_out(0x51);
 }
 
 /*
@@ -69,11 +77,11 @@ void lcd_init()
 */
 void lcd_out(int pos, unsigned char *s)
 {
-    sci_out((unsigned char) 0xfe);   // Set the cursor position
-    sci_out((unsigned char) 0x45);
-    sci_out((unsigned char) pos);
+	sci_out((unsigned char) 0xfe);	// Set the cursor position
+	sci_out((unsigned char) 0x45);
+	sci_out((unsigned char) pos);
 
-    sci_outs(s);                     // Output the string
+	sci_outs(s);							// Output the string
 }
 
 /* ----------------------------------------------------------------------- */
@@ -82,29 +90,30 @@ void lcd_out(int pos, unsigned char *s)
   sci_init - Initialize the SCI port
 */
 void sci_init(void) {
-    UBRR0 = MYUBRR;          // Set baud rate
-    UCSR0B |= (1 << TXEN0);  // Turn on transmitter
-    UCSR0C = (3 << UCSZ00);  // Set for asynchronous operation, no parity, 
-                             // one stop bit, 8 data bits
+	UBRR0 = MYUBRR;				// Set baud rate
+	UCSR0B |= (1 << TXEN0);		// Turn on transmitter
+	UCSR0B |= (1 << TXEN0);		// Turn on recv
+	UCSR0C = (3 << UCSZ00);		// Set for asynchronous operation, no parity, 
+										// one stop bit, 8 data bits
 }
 
 /*
-  sci_out - Output a byte to SCI port
+	sci_out - Output a byte to SCI port
 */
 void sci_out(unsigned char ch)
 {
-    while ( (UCSR0A & (1<<UDRE0)) == 0);
-    UDR0 = ch;
+	while ( (UCSR0A & (1<<UDRE0)) == 0);
+	UDR0 = ch;
 }
 
 /*
-  sci_outs - Print the contents of the character string "s" out the SCI
-  port. The string must be terminated by a zero byte.
+	sci_outs - Print the contents of the character string "s" out the SCI
+	port. The string must be terminated by a zero byte.
 */
 void sci_outs(unsigned char *s)
 {
-    unsigned char ch;
+	unsigned char ch;
 
-    while ((ch = *s++) != (unsigned char) '\0')
-        sci_out(ch);
+	while ((ch = *s++) != (unsigned char) '\0')
+		sci_out(ch);
 }
